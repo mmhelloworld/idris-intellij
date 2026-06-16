94ddc838718e882a497d8b38c5fb4ef58054198e
Marimuthu Madasamy <mmhelloworld@gmail.com>
2026-06-16 13:56:07 -0400

    Fix read oction error

--- src/main/kotlin/io/github/mmhelloworld/idrisintellij/ide/IdrisRootResolver.kt
+++ src/main/kotlin/io/github/mmhelloworld/idrisintellij/ide/IdrisRootResolver.kt
@@ -1,5 +1,6 @@
 package io.github.mmhelloworld.idrisintellij.ide
 
+import com.intellij.openapi.application.ReadAction
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.roots.ProjectFileIndex
 import com.intellij.openapi.vfs.VirtualFile
@@ -14,18 +15,18 @@ import java.nio.file.Paths
  */
 object IdrisRootResolver {
 
-    fun rootFor(project: Project, file: VirtualFile): Path {
+    fun rootFor(project: Project, file: VirtualFile): Path = ReadAction.compute<Path, RuntimeException> {
         var dir: VirtualFile? = file.parent
         while (dir != null) {
             if (dir.children.any { !it.isDirectory && it.extension == "ipkg" }) {
-                return Paths.get(dir.path)
+                return@compute Paths.get(dir.path)
             }
             if (dir.path == project.basePath) break
             dir = dir.parent
         }
         val contentRoot = ProjectFileIndex.getInstance(project).getContentRootForFile(file)
-        if (contentRoot != null) return Paths.get(contentRoot.path)
-        return Paths.get(file.parent?.path ?: project.basePath ?: ".")
+        if (contentRoot != null) return@compute Paths.get(contentRoot.path)
+        Paths.get(file.parent?.path ?: project.basePath ?: ".")
     }
 
     fun relativePath(root: Path, file: VirtualFile): String =
